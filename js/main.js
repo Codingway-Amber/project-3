997bea72cd67048f9c5bb06c2241970abea48f9b
Codingway-Amber <amberreine7@gmail.com>
2024-10-31 00:48:36 -0500

    sizes adjusted

--- js/main.js
+++ js/main.js
@@ -42,258 +42,104 @@ class Glitch {
         this.channelLen = 4;
         this.imgOrigin = img;
         this.imgOrigin.loadPixels();
-        this.copyData = [];
-        this.flowLineImgs = [];
-        this.shiftLineImgs = [];
-        this.shiftRGBs = [];
-        this.scatImgs = [];
-        this.throughFlag = true;
         this.copyData = new Uint8ClampedArray(this.imgOrigin.pixels);
 
-        // flow line
-        for (let i = 0; i < 1; i++) {
-            let o = {
-                pixels: null,
-                t1: floor(random(0, 1000)),
-                speed: floor(random(4, 24)),
-                randX: floor(random(24, 80))
-            };
-            this.flowLineImgs.push(o);
-        }
-
-        // shift line
-        for (let i = 0; i < 6; i++) {
-            let o = null;
-            this.shiftLineImgs.push(o);
-        }
-
-        // shift RGB
-        for (let i = 0; i < 1; i++) {
-            let o = null;
-            this.shiftRGBs.push(o);
-        }
-
-        // scat imgs
-        for (let i = 0; i < 3; i++) {
-            let scatImg = {
-                img: null,
-                x: 0,
-                y: 0
-            };
-            this.scatImgs.push(scatImg);
-        }
+        // Array setup for glitch effects
+        this.flowLineImgs = [{ t1: floor(random(0, 1000)), speed: floor(random(4, 24)), randX: floor(random(24, 80)) }];
+        this.shiftLineImgs = Array(6).fill(null);
+        this.shiftRGBs = Array(1).fill(null);
+        this.scatImgs = Array(3).fill({ img: null, x: 0, y: 0 });
     }
 
     replaceData(destImg, srcPixels) {
-        for (let y = 0; y < destImg.height; y++) {
-            for (let x = 0; x < destImg.width; x++) {
-                let r, g, b, a;
-                let index;
-                index = (y * destImg.width + x) * this.channelLen;
-                r = index;
-                g = index + 1;
-                b = index + 2;
-                a = index + 3;
-                destImg.pixels[r] = srcPixels[r];
-                destImg.pixels[g] = srcPixels[g];
-                destImg.pixels[b] = srcPixels[b];
-                destImg.pixels[a] = srcPixels[a];
-            }
+        for (let i = 0; i < destImg.pixels.length; i++) {
+            destImg.pixels[i] = srcPixels[i];
         }
         destImg.updatePixels();
     }
 
     flowLine(srcImg, obj) {
-
-        let destPixels,
-            tempY;
-        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        let destPixels = new Uint8ClampedArray(srcImg.pixels);
         obj.t1 %= srcImg.height;
         obj.t1 += obj.speed;
-        //tempY = floor(noise(obj.t1) * srcImg.height);
-        tempY = floor(obj.t1);
-        for (let y = 0; y < srcImg.height; y++) {
-            if (tempY === y) {
-                for (let x = 0; x < srcImg.width; x++) {
-                    let r, g, b, a;
-                    let index;
-                    index = (y * srcImg.width + x) * this.channelLen;
-                    r = index;
-                    g = index + 1;
-                    b = index + 2;
-                    a = index + 3;
-                    destPixels[r] = srcImg.pixels[r] + obj.randX;
-                    destPixels[g] = srcImg.pixels[g] + obj.randX;
-                    destPixels[b] = srcImg.pixels[b] + obj.randX;
-                    destPixels[a] = srcImg.pixels[a];
-                }
-            }
+        let tempY = floor(obj.t1);
+        for (let x = 0; x < srcImg.width; x++) {
+            let index = (tempY * srcImg.width + x) * this.channelLen;
+            destPixels[index] = srcImg.pixels[index] + obj.randX;
+            destPixels[index + 1] = srcImg.pixels[index + 1] + obj.randX;
+            destPixels[index + 2] = srcImg.pixels[index + 2] + obj.randX;
         }
         return destPixels;
     }
 
     shiftLine(srcImg) {
-
-        let offsetX;
-        let rangeMin, rangeMax;
-        let destPixels;
-        let rangeH;
-
-        destPixels = new Uint8ClampedArray(srcImg.pixels);
-        rangeH = srcImg.height;
-        rangeMin = floor(random(0, rangeH));
-        rangeMax = rangeMin + floor(random(1, rangeH - rangeMin));
-        offsetX = this.channelLen * floor(random(-40, 40));
-
-        for (let y = 0; y < srcImg.height; y++) {
-            if (y > rangeMin && y < rangeMax) {
-                for (let x = 0; x < srcImg.width; x++) {
-                        let r, g, b, a;
-                        let r2, g2, b2, a2;
-                        let index;
-
-                        index = (y * srcImg.width + x) * this.channelLen;
-                        r = index;
-                        g = index + 1;
-                        b = index + 2;
-                        a = index + 3;
-                        r2 = r + offsetX;
-                        g2 = g + offsetX;
-                        b2 = b + offsetX;
-                        destPixels[r] = srcImg.pixels[r2];
-                        destPixels[g] = srcImg.pixels[g2];
-                        destPixels[b] = srcImg.pixels[b2];
-                        destPixels[a] = srcImg.pixels[a];
-                }
+        let destPixels = new Uint8ClampedArray(srcImg.pixels);
+        let rangeH = srcImg.height;
+        let rangeMin = floor(random(0, rangeH));
+        let rangeMax = rangeMin + floor(random(1, rangeH - rangeMin));
+        let offsetX = this.channelLen * floor(random(-40, 40));
+        for (let y = rangeMin; y < rangeMax; y++) {
+            for (let x = 0; x < srcImg.width; x++) {
+                let index = (y * srcImg.width + x) * this.channelLen;
+                let r2 = index + offsetX;
+                destPixels[index] = srcImg.pixels[r2] || 0;
             }
         }
         return destPixels;
     }
 
     shiftRGB(srcImg) {
-
-        let randR, randG, randB;
-        let destPixels;
-        let range;
-
-        range = 16;
-        destPixels = new Uint8ClampedArray(srcImg.pixels);
-        randR = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
-        randG = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
-        randB = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
-
-        for (let y = 0; y < srcImg.height; y++) {
-            for (let x = 0; x < srcImg.width; x++) {
-                let r, g, b, a;
-                let r2, g2, b2, a2;
-                let index;
-
-                index = (y * srcImg.width + x) * this.channelLen;
-                r = index;
-                g = index + 1;
-                b = index + 2;
-                a = index + 3;
-                r2 = (r + randR) % srcImg.pixels.length;
-                g2 = (g + randG) % srcImg.pixels.length;
-                b2 = (b + randB) % srcImg.pixels.length;
-                destPixels[r] = srcImg.pixels[r2];
-                destPixels[g] = srcImg.pixels[g2];
-                destPixels[b] = srcImg.pixels[b2];
-                destPixels[a] = srcImg.pixels[a];
-            }
+        let destPixels = new Uint8ClampedArray(srcImg.pixels);
+        let randR = (floor(random(-16, 16)) * srcImg.width + floor(random(-16, 16))) * this.channelLen;
+        let randG = (floor(random(-16, 16)) * srcImg.width + floor(random(-16, 16))) * this.channelLen;
+        let randB = (floor(random(-16, 16)) * srcImg.width + floor(random(-16, 16))) * this.channelLen;
+        for (let i = 0; i < srcImg.pixels.length; i += this.channelLen) {
+            destPixels[i] = srcImg.pixels[(i + randR) % srcImg.pixels.length];
+            destPixels[i + 1] = srcImg.pixels[(i + randG) % srcImg.pixels.length];
+            destPixels[i + 2] = srcImg.pixels[(i + randB) % srcImg.pixels.length];
         }
-
         return destPixels;
     }
 
     getRandomRectImg(srcImg) {
-        let startX;
-        let startY;
-        let rectW;
-        let rectH;
-        let destImg;
-        startX = floor(random(0, srcImg.width - 30));
-        startY = floor(random(0, srcImg.height - 50));
-        rectW = floor(random(30, srcImg.width - startX));
-        rectH = floor(random(1, 50));
-        destImg = srcImg.get(startX, startY, rectW, rectH);
-        destImg.loadPixels();
-        return destImg;
+        let startX = floor(random(0, srcImg.width - 30));
+        let startY = floor(random(0, srcImg.height - 50));
+        let rectW = floor(random(30, srcImg.width - startX));
+        let rectH = floor(random(1, 50));
+        return srcImg.get(startX, startY, rectW, rectH);
     }
 
     show() {
-        // Restore the original state of the image
         this.replaceData(this.imgOrigin, this.copyData);
-    
-        // Calculate centered position after scaling
-        const centerX = (width - this.imgOrigin.width * 0.5) / 2;
-        const centerY = (height - this.imgOrigin.height * 0.5) / 2;
-    
-        // Sometimes pass without effect processing
+
+        const centerX = (width - this.imgOrigin.width * 0.45) / 2;
+        const centerY = (height - this.imgOrigin.height * 0.45) / 2;
+
         let n = floor(random(100));
-        if (n > 75 && this.throughFlag) {
-            this.throughFlag = false;
-            setTimeout(() => {
-                this.throughFlag = true;
-            }, floor(random(40, 400)));
-        }
-    
-        // Draw without glitch
-        if (!this.throughFlag) {
-            push();
-            translate(centerX, centerY);
-            scale(0.5);
-            image(this.imgOrigin, 0, 0);
-            pop();
+        if (n > 75) {
+            image(this.imgOrigin, centerX, centerY, this.imgOrigin.width * 0.45, this.imgOrigin.height * 0.45);
             return;
         }
-    
-        // Apply glitch effects (flow lines, shift lines, etc.)
-        this.flowLineImgs.forEach((v, i, arr) => {
-            arr[i].pixels = this.flowLine(this.imgOrigin, v);
-            if (arr[i].pixels) {
-                this.replaceData(this.imgOrigin, arr[i].pixels);
-            }
-        });
-        this.shiftLineImgs.forEach((v, i, arr) => {
-            if (floor(random(100)) > 50) {
-                arr[i] = this.shiftLine(this.imgOrigin);
-                this.replaceData(this.imgOrigin, arr[i]);
-            } else {
-                if (arr[i]) {
-                    this.replaceData(this.imgOrigin, arr[i]);
-                }
-            }
-        });
-        this.shiftRGBs.forEach((v, i, arr) => {
-            if (floor(random(100)) > 65) {
-                arr[i] = this.shiftRGB(this.imgOrigin);
-                this.replaceData(this.imgOrigin, arr[i]);
-            }
-        });
-    
-        // Draw glitch effect
-        push();
-        translate(centerX, centerY);
-        scale(0.5);
-        image(this.imgOrigin, 0, 0);
-        pop();
-    
-        // Additional scatter glitch effect
-        this.scatImgs.forEach((obj) => {
-            push();
-            translate(centerX, centerY);
-            scale(0.5);
-            if (floor(random(100)) > 80) {
-                obj.x = floor(random(-this.imgOrigin.width * 0.3, this.imgOrigin.width * 0.7));
-                obj.y = floor(random(-this.imgOrigin.height * 0.1, this.imgOrigin.height));
-                obj.img = this.getRandomRectImg(this.imgOrigin);
-            }
-            if (obj.img) {
-                image(obj.img, obj.x, obj.y);
-            }
-            pop();
-        });
-    }      
 
+        this.flowLineImgs.forEach(v => {
+            let pixels = this.flowLine(this.imgOrigin, v);
+            if (pixels) this.replaceData(this.imgOrigin, pixels);
+        });
+        this.shiftLineImgs.forEach((v, i) => {
+            if (floor(random(100)) > 50) {
+                this.shiftLineImgs[i] = this.shiftLine(this.imgOrigin);
+                this.replaceData(this.imgOrigin, this.shiftLineImgs[i]);
+            }
+        });
+        this.shiftRGBs.forEach((v, i) => {
+            if (floor(random(100)) > 65) {
+                this.shiftRGBs[i] = this.shiftRGB(this.imgOrigin);
+                this.replaceData(this.imgOrigin, this.shiftRGBs[i]);
+            }
+        });
+
+        push();
+        image(this.imgOrigin, centerX, centerY, this.imgOrigin.width * 0.45, this.imgOrigin.height * 0.45);
+        pop();
+    }
 }
